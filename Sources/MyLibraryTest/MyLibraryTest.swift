f1ee09ad0b7052372ebba2a51d8e9eb44ab7fe01
Tilak <tilak20082009@gmail.com>
2021-08-26 23:16:54 +0530

    test

--- Sources/MyLibraryTest/MyLibraryTest.swift
+++ Sources/MyLibraryTest/MyLibraryTest.swift
@@ -1,3 +1,11 @@
 struct MyLibraryTest {
     var text = "Hello, World!"
 }
+
+public class TestPackage {
+    let  stPackage = MyLibraryTest(text: "Hello,Welcome to the Test Package")
+    public func PRINT()  {
+        print(stPackage.text)
+    }
+    
+}
